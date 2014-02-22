b211a51c12125de83cc59225b70a597006ba92f8
eyelashjune <june@eyemee.com>
2014-02-22 20:04:50 +0900

    Finished Chapter 6.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
 	validates :name,	presence: true,
 						length: { maximum: 50 }
 
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	#VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	#following REGEX is more advanced to prevent double-dots in address such as foo@bar..com
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email,	presence: true,
 						format: { with: VALID_EMAIL_REGEX },
 						uniqueness: { case_sensitive: false }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -70,6 +70,22 @@ describe User do
   	it { should_not be_valid }
   end
 
+  describe "when email address includes double dots" do
+  	before { @user.email = "foo@bar..com" }
+
+  	it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+  	let(:mixed_case_email) { "Foo@ExAMple.cOm" }
+
+  	it "should be saved as all lower-case" do
+  		@user.email = mixed_case_email
+  		@user.save
+  		expect(@user.reload.email).to eq mixed_case_email.downcase
+  	end
+  end
+
   describe "when password is not present" do
   	before do
   		@user.password = " "
